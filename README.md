d5deb2b1b35c7f2d7561dfe864187f76faf0179d
Ron Goodman <rawhideron@gmail.com>
2026-01-29 20:31:47 -0500

    recreating the kubernetes cluster

--- README.md
+++
@@ -1,6 +0,0 @@
-sudo snap install kubectl --classic
-kind create cluster --config kind-config.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl -n ingress-nginx patch deployment ingress-nginx-controller --type='merge' -p '{"spec":{"template":{"spec":{"nodeSelector":{"kubernetes.io/os":"linux","node-role.kubernetes.io/control-plane":""}}}}}'
-kubectl get nodes
-kubectl -n ingress-nginx get pods
---
+++ metrics-server.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm repo update
+
+release_name="metrics-server"
+namespace="kube-system"
+chart="metrics-server/metrics-server"
+args="{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP}"
+
+if helm status "$release_name" --namespace "$namespace" >/dev/null 2>&1; then
+  helm upgrade "$release_name" "$chart" \
+    --namespace "$namespace" \
+    --set args="$args"
+else
+  helm install "$release_name" "$chart" \
+    --namespace "$namespace" \
+    --set args="$args"
+fi
